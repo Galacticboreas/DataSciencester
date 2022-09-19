2dbd3e616b73eb9058d2c0de3c0023e46a1e9fb4
Galacticboreas <ivanovvolgdonbor@mail.ru>
2022-09-20 01:10:27 +0300

    avg_connections

--- Chapter_1/page_27.py
+++ Chapter_1/page_27.py
@@ -24,3 +24,16 @@ for i, j in friendships_f:
     friendships[j].append(i) # Добавить i как друга для j
 # {0: [1, 2], 1: [0, 2, 3], 2: [0, 1, 3], 3: [1, 2, 4], 4: [3, 5], 5: [4, 6, 7], 6: [5, 8], 
 # 7: [5, 8], 8: [6, 7, 9], 9: [8]}
+
+# число друзей
+def number_of_friends(user):
+    """Сколько друзей есть у пользователя user?"""
+    user_id = user["id"]
+    friend_ids = friendships[user_id]
+    return len(friend_ids)
+total_connections = sum(number_of_friends(user) for user in users)
+# Суммарное число связей 24
+
+num_users = len(users) # Длина списка пользователей
+avg_connections = total_connections / num_users # 24 / 10 = 2.4
+
