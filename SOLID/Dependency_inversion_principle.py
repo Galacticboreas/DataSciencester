89e86e98b6817f7c39c665ba0104cf8e3a27041f
Galacticboreas <ivanovvolgdonbor@mail.ru>
2023-09-16 22:39:52 +0300

    SOLID - DIP introduction

---
+++ SOLID/Dependency_inversion_principle.py
@@ -0,0 +1,15 @@
+"""
+OOP - every class and method should depend on
+abstractions, not concrete implementations.
+FP - every function should depend on input
+arguments only, not concrete operations.
+"""
+
+# According to this principle, all modules - regardless 
+# of their levels - should depend not on specific 
+# implementations, but on abstractions. Any dependence 
+# on specific implementations is a direct violation of 
+# the dependency inversion principle.
+
+
+
