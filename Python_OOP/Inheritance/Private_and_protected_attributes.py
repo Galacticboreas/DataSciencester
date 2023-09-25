ce7116f6f0b488738e3262fbd2d74cb93ce18092
Galacticboreas <ivanovvolgdonbor@mail.ru>
2023-09-25 21:33:57 +0300

    OOP - private and protected attributes

--- Python_OOP/Inheritance/Private_and_protected_attributes.py
+++ Python_OOP/Inheritance/Private_and_protected_attributes.py
@@ -19,4 +19,28 @@ print(r.__dict__)
 # {'_Geom__x1': 0, '_Geom__y1': 0, '_Geom__x2': 10, 
 # '_Geom__y2': 20, '_Rect__fill': 'red'}
 
+class Geom2:
+    name = "Geom2"
 
+    def __init__(self, x1, y1, x2, y2):
+        print(f"Geom2 initializer for {self.__class__}")
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+
+
+class Rect2(Geom2):
+    def __init__(self, x1, y1, x2, y2, fill='red'):
+        super().__init__(x1, y1, x2, y2)
+        self._fill = fill
+ 
+    def get_coords(self):
+        return (self._x1, self._y1, self._x2, self._y2)
+
+r = Rect2(0, 0, 10, 20)
+print(r.__dict__)
+# Geom2 initializer for <class '__main__.Rect2'>
+# {'_x1': 0, '_y1': 0, '_x2': 10, '_y2': 20, '_fill': 'red'}
+
+r.get_coords()
