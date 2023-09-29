2647cfb3dce7fd7c5ad8c1cd1fc8fc6b903bde87
Galacticboreas <ivanovvolgdonbor@mail.ru>
2023-09-29 21:47:43 +0300

    GUI - creating menus and submenus

--- GUI_wxPython/Creating_menus_and_submenus.py
+++ GUI_wxPython/Creating_menus_and_submenus.py
@@ -8,6 +8,9 @@
 import wx
 
 APP_EXIT = 1
+VIEW_STATUS = 2
+VIEW_RGB = 3
+VIEW_SRGB = 4
 
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
@@ -16,15 +19,33 @@ class MyFrame(wx.Frame):
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
 
-        # item = wx.MenuItem(fileMenu, wx.ID_EXIT, "Exit", "Exiting the application")
-        # fileMenu.Append(item)
+        expMenu = wx.Menu()
+        expMenu.Append(wx.ID_ANY, "Export image")
+        expMenu.Append(wx.ID_ANY, "Export vide")
+        expMenu.Append(wx.ID_ANY, "Export data")
 
-        item = fileMenu.Append(wx.ID_EXIT, "Exit\tCtrl+Q", "Exit the application")
+        fileMenu.Append(wx.ID_NEW, '&New\tCtrl+N')
+        fileMenu.Append(wx.ID_OPEN, '&Open\tCtrl+O')
+        fileMenu.Append(wx.ID_SAVE, '&Save\tCtrl+S')
+        fileMenu.AppendSubMenu(expMenu, "&Export")
+        fileMenu.AppendSeparator()
+
+        item = wx.MenuItem(fileMenu, APP_EXIT, "Exit\tCtrl+Q", "Exiting the application")
+        fileMenu.Append(item)
+
+        viewMenu = wx.Menu()
+        viewMenu.Append(VIEW_STATUS, "Status bar", kind=wx.ITEM_CHECK)
+        viewMenu.Append(VIEW_RGB, "Type RGB", "Type RGB", kind=wx.ITEM_RADIO)
+        viewMenu.Append(VIEW_SRGB, "Type sRGB", "Type sRGB", kind=wx.ITEM_RADIO)
+
+        # item = fileMenu.Append(wx.ID_EXIT, "Exit\tCtrl+Q", "Exit the application")
 
         menubar.Append(fileMenu, "&File")
+        menubar.Append(viewMenu, "&Type")
+
         self.SetMenuBar(menubar)
 
-        self.Bind(wx.EVT_MENU, self.onQuit, item)
+        self.Bind(wx.EVT_MENU, self.onQuit, id=APP_EXIT)
 
     def onQuit(self, event):
         self.Close()
